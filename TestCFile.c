275a952ddcf926ff6449eb54f3337882899cbbf8
vikhani <51340623+vikhani@users.noreply.github.com>
2020-03-25 14:31:25 +0300

    Add files via upload

---
+++ TestCFile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Foo()
+{
+	const char *home;
+	home = getenv("HOME");
+	if (home != NULL)
+	{
+	  char *buff;
+	  buff = malloc(strlen(home) + strlen("/.local/")+1);
+	  strcpy(buff, home);
+	  strcat(buff, "/.local/share/");
+	  free(buff);
+	}
+}
